7c344243fca44d64794003cb372a3141d92a5e7e
Seu NomeRenato <renatofatore@gmail.com>
2024-11-08 19:31:09 -0300

    Primeiro commit - adicionando programa de soma

---
+++ somar.py
@@ -0,0 +1,9 @@
+def somar(a, b):
+    resultado = a + b
+    return resultado
+
+
+numero1 = int(input("Digite o primeiro número cara:"))
+numero2 = int(input("Digite o segundo número cara:"))
+
+print(somar(numero1, numero2))
